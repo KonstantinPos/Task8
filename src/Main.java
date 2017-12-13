e804f0b078ccad02e3d6420bda3447ceb77d7416
KonstantinPos <posusaevkostay@mail.ru>
2017-12-13 17:41:03 +0300

    Task

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
+        String n = scanner.nextLine();
+        System.out.println("Количество веденных чисел равно = " + n.length());
+
+    }
+}
